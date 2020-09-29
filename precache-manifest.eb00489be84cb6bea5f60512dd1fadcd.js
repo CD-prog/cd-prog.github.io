5db4b53b6d20cc7cfda11c4777347de1313916e6
CD-prog <61663007+CD-prog@users.noreply.github.com>
2020-09-29 09:15:43 -0500

    Updates

---
+++ precache-manifest.eb00489be84cb6bea5f60512dd1fadcd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "406d1e006b1d08a9f3ae2e7c73630d4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adb8222e68706a6df874",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a256c2803f105fd265ea",
+    "url": "/static/css/main.5757957e.chunk.css"
+  },
+  {
+    "revision": "adb8222e68706a6df874",
+    "url": "/static/js/2.dbfd447b.chunk.js"
+  },
+  {
+    "revision": "596676716ca1328e2733d607535d6ca7",
+    "url": "/static/js/2.dbfd447b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a256c2803f105fd265ea",
+    "url": "/static/js/main.0df0518f.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "de7ab852e6817dcd0ab844c8844a321e",
+    "url": "/static/media/burger.de7ab852.png"
+  },
+  {
+    "revision": "0f503bf0b1861b38dc81006812bf5c86",
+    "url": "/static/media/image.0f503bf0.png"
+  },
+  {
+    "revision": "43917e4b5930acc44af74dbf0bdcef9d",
+    "url": "/static/media/linkedin.43917e4b.jpg"
+  },
+  {
+    "revision": "eb79da251458bd15efe94f7b144f771d",
+    "url": "/static/media/profile-photo.eb79da25.JPG"
+  },
+  {
+    "revision": "3d4111fadd8d5b05c8bd248c27bb0560",
+    "url": "/static/media/resume.3d4111fa.pdf"
+  },
+  {
+    "revision": "0d55b663b9ea28fb7bdf5b414ec7df0b",
+    "url": "/static/media/screen-shot-5.0d55b663.png"
+  },
+  {
+    "revision": "9239f3940cebb62b8a56217ee118c948",
+    "url": "/static/media/screen-shot.9239f394.png"
+  },
+  {
+    "revision": "425b2de4d1ceabdd2ee30ec549c24586",
+    "url": "/static/media/trivia-night.425b2de4.png"
+  },
+  {
+    "revision": "c19ae5f6c56d29dcdc4be7a17abebb55",
+    "url": "/static/media/work-day.c19ae5f6.png"
+  }
+]);
--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default () => {
-    return(
-        <footer className="navbar sticky-bottom navbar-expand-md navbar-dark bg-dark">
-        <a className="navbar-brand mx-auto text-center" href="/">© Copyright 2020</a>
-      </footer>
-    )
-}
