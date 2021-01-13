32832947d7853e2b93295310b173359ce9d802b0
CD-prog <61663007+CD-prog@users.noreply.github.com>
2021-01-13 12:57:46 -0600

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default () => {
     return(
         <footer className="navbar sticky-bottom navbar-expand-md navbar-dark bg-dark">
-        <a className="navbar-brand mx-auto text-center" href="/">© Copyright 2020 | Claudiu Diaconeasa</a>
+        <a className="navbar-brand mx-auto text-center" href="/">© Copyright 2021| Claudiu Diaconeasa</a>
       </footer>
     )
 }
